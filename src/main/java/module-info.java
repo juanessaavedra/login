6adee515d4a7da8a1e0175d9218bcd83887773a9
arle morales 23 <87079423+arleth64@users.noreply.github.com>
2023-05-22 16:11:16 -0500

    commit: upload javaFX example

---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module com.example.ejemplocrud {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.core;
+    requires java.sql;
+    requires mysql.connector.j;
+
+    opens com.example.ejemplocrud to javafx.fxml;
+    opens library to javafx.fxml;
+    exports com.example.ejemplocrud;
+    exports library;
+}
